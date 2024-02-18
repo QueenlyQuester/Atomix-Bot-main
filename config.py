105c3faa7e8ff43b09d76e42e5f7b3e09a01c6ea
QueenlyQuester <thepotato5559@gmail.com>
2024-02-18 18:13:42 +0100

    Removed my Bot Token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Final
 
-TOKEN: Final = "MTE5MjUzMjc1NTMzNTA4NjEzMA.GkfAM5._JU6KF4slgj-VPlmcjR-2lZPiJiYeYo6IBXLag"
+TOKEN: Final = "your_bot_token"
 
 JOIN_CHANNEL_ID: Final = "1200187124041912390"
