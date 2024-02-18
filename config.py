4c8923b700f241df706ea93ae8e9d4c5d36d41c0
QueenlyQuester <thepotato5559@gmail.com>
2024-02-18 18:13:04 +0100

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Final
 
-TOKEN: Final = "your_bot_token"
+TOKEN: Final = "MTE5MjUzMjc1NTMzNTA4NjEzMA.GkfAM5._JU6KF4slgj-VPlmcjR-2lZPiJiYeYo6IBXLag"
 
 JOIN_CHANNEL_ID: Final = "1200187124041912390"
