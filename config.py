d3653db57e77f245cbf45435c207da5fd4c1c9ad
QueenlyQuester <thepotato5559@gmail.com>
2024-02-26 18:45:28 +0100

    remove tickets cog, because it wasn't working

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Final
 
-TOKEN: Final = "your_bot_token"
+TOKEN: Final = "MTE5MjUzMjc1NTMzNTA4NjEzMA.GzvkRC.Hhx-846CyiT7A-y8jvF0DflvTHIOuqFYrakXxc"
 
 JOIN_CHANNEL_ID: Final = "1210590436872294441"  
