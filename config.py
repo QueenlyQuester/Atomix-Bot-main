e4bee14fecc3b8a2f3ad68f196dd8a5f56333685
Aetherium <130498201+QueenlyQuester@users.noreply.github.com>
2024-02-26 18:47:15 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Final
 
-TOKEN: Final = "MTE5MjUzMjc1NTMzNTA4NjEzMA.GzvkRC.Hhx-846CyiT7A-y8jvF0DflvTHIOuqFYrakXxc"
+TOKEN: Final = "your_bot_token"
 
 JOIN_CHANNEL_ID: Final = "1210590436872294441"  
